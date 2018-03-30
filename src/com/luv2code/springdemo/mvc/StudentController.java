91ed24bf19f03394184cb36baff777ba9b0b037e
larry <ladams1776@yahoo.com>
2018-03-29 21:41:35 -0400

    built form and the processForm method on the controller.

--- src/com/luv2code/springdemo/mvc/StudentController.java
+++ src/com/luv2code/springdemo/mvc/StudentController.java
@@ -2,6 +2,7 @@ package com.luv2code.springdemo.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,6 +21,18 @@ public class StudentController {
 
 		return "student-form";
 	}
+	
+	
+	
+	@RequestMapping("/processForm")
+	public String processForm(@ModelAttribute("student") Student theStudent) {
+		
+		// log the input data
+		System.out.println("The student: " + theStudent.getFirstName() + " " + theStudent.getLastName());
+		return "student-confirmation";
+	}
+	
+	
 }
 
 
