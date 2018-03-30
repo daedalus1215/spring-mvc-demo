3537e8e29ecc19fa4c1d9a0699ec342bcf427aa7
larry <ladams1776@yahoo.com>
2018-03-29 20:47:55 -0400

    @RequestParam is working correctly.

--- src/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -52,17 +53,17 @@ public class HelloWorldController {
 	
 	
 	
-	
+	// Third testing set.
 	@RequestMapping("/processFormVersionThree")
-	public String processFormVersionThree(HttpServletRequest request, Model model) {
-		// read the request parameter from the HTML form	
-		String theName = request.getParameter("studentName");
+	public String processFormVersionThree(
+			@RequestParam("studentName") String theName, 
+			Model model) {	
 		
 		// convert the data to all caps
 		theName = theName.toUpperCase();
 		
 		// create the message
-		String result = "Yo " + theName;
+		String result = "RequestParameter in method signature working for " + theName;
 		
 		// add message to the model
 		model.addAttribute("message", result);
