2ebc2f1a9b4e615faaefeab541cca9bdb9950a50
larry <ladams1776@yahoo.com>
2018-04-04 06:33:32 -0400

    adding hte validation attributes to the Customer class.

--- src/com/luv2code/springdemo/mvc/Customer.java
+++ src/com/luv2code/springdemo/mvc/Customer.java
@@ -1,5 +1,7 @@
 package com.luv2code.springdemo.mvc;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -14,6 +16,9 @@ public class Customer {
 	@Size(min=2, message="Length is too small for last name")
 	private String lastName;
 
+	@Min(value=0, message="must be greater than or equal to zero")
+	@Max(value=10, message="must be less than or equal to 10")
+	private Integer freePasses;
 	
 	
 	
@@ -32,4 +37,12 @@ public class Customer {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
+	public Integer getFreePasses() {
+		return freePasses;
+	}
+
+	public void setFreePasses(Integer freePasses) {
+		this.freePasses = freePasses;
+	}	
 }
