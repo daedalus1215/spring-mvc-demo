ac0878de1f36f6fce04783f81c616f076f2d9024
larry <ladams1776@yahoo.com>
2018-04-05 06:14:14 -0400

    using a Annotation Filtering on Customer postalCode via @Pattern annotation + regexp argument.

--- src/com/luv2code/springdemo/mvc/Customer.java
+++ src/com/luv2code/springdemo/mvc/Customer.java
@@ -3,6 +3,7 @@ package com.luv2code.springdemo.mvc;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 
@@ -20,6 +21,9 @@ public class Customer {
 	@Min(value=0, message="must be greater than or equal to zero")
 	@Max(value=10, message="must be less than or equal to 10")
 	private Integer freePasses;
+	
+	@Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 chars/digits")
+	private String postalCode;
 	
 	
 	
@@ -45,5 +49,13 @@ public class Customer {
 
 	public void setFreePasses(Integer freePasses) {
 		this.freePasses = freePasses;
-	}	
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}		
 }
