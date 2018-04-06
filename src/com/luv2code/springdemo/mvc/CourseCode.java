43755d0085065184eff0088af2d31e55f321ba9e
larry <ladams1776@yahoo.com>
2018-04-06 06:25:05 -0400

    created CourseCodeConstraintValidator

--- src/com/luv2code/springdemo/mvc/CourseCode.java
+++ src/com/luv2code/springdemo/mvc/CourseCode.java
@@ -7,9 +7,14 @@ import java.lang.annotation.Target;
 
 import javax.validation.Constraint;
 
-@Constraint(validatedBy=CourseCodeConstraintValidator.class)
+@Constraint(validatedBy = CourseCodeConstraintValidator.class)
 @Target( { ElementType.METHOD, ElementType.FIELD } ) // can use this constraint on a method or a field
 @Retention(RetentionPolicy.RUNTIME)
 public @interface CourseCode {
 	
+	// define default course code
+	public String value() default "LUV";
+	
+	// define default error message
+	public String message() default "must start with LUV";
 }
